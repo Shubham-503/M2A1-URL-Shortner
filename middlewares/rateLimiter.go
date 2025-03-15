a18e80a8e7051bb44d5160a72625d4a4984109be
Shubham-503 <kr.shubham025@gmail.com>
2025-03-15 20:08:40 +0530

    Added Free tier middleware

--- middlewares/rateLimiter.go
+++ middlewares/rateLimiter.go
@@ -67,3 +67,35 @@ func APIRateLimitMiddleware(maxRequest int) func(http.Handler) http.Handler {
 		})
 	}
 }
+
+func FreeTierMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("api_key")
+		if apiKey == "" || apiKey == "free" {
+
+			ip := getIPAddress(r)
+			key := "rate:" + ip
+
+			ctx := RateLimitRedisStore.Ctx
+
+			count, err := RateLimitRedisStore.Client.Incr(ctx, key).Result()
+			if err != nil {
+				// In case of error, let the request pass.
+				next.ServeHTTP(w, r)
+				return
+			}
+			// If this is the first request, set an expiry of 1 minute.
+			if count == 1 {
+				RateLimitRedisStore.Client.Expire(ctx, key, time.Minute)
+			}
+
+			// If the number of requests exceeds 100, throttle the request.
+			if count >= 5 {
+				w.WriteHeader(http.StatusTooManyRequests)
+				w.Write([]byte("Rate limit exceeded for free tier. Please upgrade your plan or try again later."))
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
